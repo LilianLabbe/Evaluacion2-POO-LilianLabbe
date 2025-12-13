bc1e89284c093cf5617364c9772d76d0ddd6d128
LilianLabbe <lilian.labbe@inacapmail.cl>
2025-12-13 19:34:18 -0300

    main.py

--- Ejercicio 2/main.py
+++ Ejercicio 2/main.py
@@ -5,9 +5,9 @@ from empresa import Empresa
 
 def main():
     empresa = Empresa()
-    empresa.agregar(Vendedor("Andrea", "11.111.111-1", 450000, 1500000, 0.04))
-    empresa.agregar(Gerente("Diego", "22.222.222-2", 1200000, 200000))
-    empresa.agregar(Practicante("Tomás", "33.333.333-3", 60, 4500))
+    empresa.agregar(Vendedor("Lilian", "17.871.561-1", 450000, 1500000, 0.04))
+    empresa.agregar(Gerente("Benjamin", "22.472.652-2", 1200000, 200000))
+    empresa.agregar(Practicante("Edgard", "18.263.983-3", 60, 4500))
 
     print("\nTrabajadores activos:")
     for t in empresa.activos():
